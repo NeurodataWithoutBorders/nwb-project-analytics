fb20581d80d2702857e7a11c2ca7219a5ccf9d75
Oliver Ruebel <oruebel@lbl.gov>
2021-08-17 12:44:57 -0700

    Do not store summary stats in GitCodeStats class

--- src/projectstats/codestats.py
+++ src/projectstats/codestats.py
@@ -17,10 +17,17 @@ class GitCodeStats:
     >> git_code_stats.compute_code_stats(...)
     >> git_code_stats.compute_summary_stats(...)
 
+    If results have been computed previously and cached then do:
+
+    >> git_code_stats = GitCodeStats.from_cache(...)
+    >> git_code_stats.compute_summary_stats(...)
+
+    We can check if valid cached files exists via GitCodeStats.cached() or the from_cache function will raise
+    a ValueError if the cache does not exists.
+
     :ivar git_paths: Dict of strings with the keys being the name of the tool and the values being
                      the git URL, e.g,. 'https://github.com/NeurodataWithoutBorders/pynwb.git'.
     :ivar output_dir: Path to the directory where outputs are being stored
-    :ivar cloc_path: Path to the cloc command for running cloc stats
     :ivar source_dir: Path wheter the sources of repos are being checked out to. (self.output_dir/src)
     :ivar cache_file_cloc: Path to the YAML file for storing cloc statistics (may not exist if results are not cached)
     :ivar cache_file_commits: Path to the YAML file with the commit stats (may not exist if results are not cached)
@@ -31,27 +38,64 @@ class GitCodeStats:
 
 
     """
-    def __init__(self, git_paths, output_dir, cloc_path):
+    def __init__(self, output_dir):
         """
-
-        :param git_paths: Dict of strings with the keys being the name of the tool and the values being
-                          the git URL, e.g,. 'https://github.com/NeurodataWithoutBorders/pynwb.git'.
         :param output_dir: Path to the directory where outputs are being stored
-        :param cloc_path: Path to the cloc command for running cloc stats
         """
-        self.git_paths = git_paths
+        self.git_paths = {}
         self.output_dir = output_dir
-        self.cloc_path = cloc_path
         self.source_dir = os.path.join(output_dir, 'src')
         self.cache_file_cloc = os.path.join(self.output_dir, 'cloc_stats.yaml')
         self.cache_file_commits = os.path.join(self.output_dir, 'commit_stats.yaml')
+        self.cache_git_paths = os.path.join(self.output_dir, 'git_paths.yaml')
         self.commit_stats = None
         self.cloc_stats = None
-        self.summary_stats = None
 
-    def compute_code_stats(self, load_cached_results=True, cache_results=True):
+    @staticmethod
+    def from_cache(output_dir):
         """
+        Create a GitCodeStats object from cached results
+        :param output_dir: The output directory where the cache files are stored
+        :return: A new GitCodeStats object with the resutls loaded from the cache
+        """
+        re = GitCodeStats(output_dir)
+        if GitCodeStats.cached(output_dir):
+            print("Loading cached results: %s" % re.cache_file_cloc)
+            with open(re.cache_file_cloc) as f:
+                re.cloc_stats = yaml.load(f, Loader=yaml.FullLoader)
+            print("Loading cached results: %s" % re.cache_file_commits)
+            with open(re.cache_file_commits) as f:
+                re.commit_stats = yaml.load(f, Loader=yaml.FullLoader)
 
+            print("Loading cached results: %s" % re.cache_git_paths)
+            with open(re.cache_git_paths) as f:
+                re.git_paths = yaml.load(f, Loader=yaml.FullLoader)
+            return re
+        raise ValueError("No cache available at %s" % output_dir)
+
+    @staticmethod
+    def cached(output_dir):
+        """Check if a cached version of this class exists at output_dir"""
+        temp = GitCodeStats(output_dir)
+        return (os.path.exists(temp.cache_file_cloc) and
+                os.path.exists(temp.cache_file_commits) and
+                os.path.exists(temp.cache_git_paths))
+
+    def compute_code_stats(self, git_paths, cloc_path, cache_results=True):
+        """
+        Compute code statistics suing CLOC.
+
+        NOTE: Repos will be checked out from GitHub and CLOC computed for all
+              commits, i.e., the repo will be checked out at all commits of the
+              repo and CLOC will be run. This process can be very expensive.
+              Using the cache is recommended when possible.
+
+        WARNING: This function calls self.clean_outdirs. Any previously cached results will be lost!
+
+
+        :param git_paths: Dict of strings with the keys being the name of the tool and the values being
+                          the git URL, e.g,. 'https://github.com/NeurodataWithoutBorders/pynwb.git'.
+        :param cloc_path: Path to the cloc command for running cloc stats
         :param load_cached_results: Boolean indicating whether results should be loaded from cache if possible or
                                     if the cache should be cleaned and results recomputed. NOTE: Setting to false
                                     will lead to calling clean_outdir to clean up results.
@@ -60,41 +104,35 @@ class GitCodeStats:
         :type cache_results: bool
         :return: None. The function initalizes self.commit_stats and self.cloc_stats
         """
-        # Load cached results if available
-        if load_cached_results and os.path.exists(self.cache_file_cloc) and os.path.exists(self.cache_file_commits):
-            print("Loading cached results: %s" % self.cache_file_cloc)
-            with open(self.cache_file_cloc) as f:
-                self.cloc_stats = yaml.load(f, Loader=yaml.FullLoader)
-            print("Loading cached results: %s" % self.cache_file_commits)
-            with open(self.cache_file_commits) as f:
-                self.commit_stats = yaml.load(f, Loader=yaml.FullLoader)
-        # Compute the results if not cached
-        else:
-            # Clean and create output directory
-            self.clean_outdirs(output_dir=self.output_dir,
-                               source_dir=self.source_dir)
-            # Clone all repos
-            print("Cloning all repos...")
-            git_repos = self.clone_repos(repos=self.git_paths, source_dir=self.source_dir)
-            # Compute CLOC and Commit statistics for all repos
-            self.commit_stats = {}
-            self.cloc_stats = {}
-            for name, repo in git_repos.items():
-                print("Compute CLOC stats: %s" % name)
-                commit_res, cloc_res = self.git_repo_stats(
-                    repo,
-                    cloc_path=self.cloc_path,
-                    output_dir=self.output_dir)
-                self.commit_stats[name] = commit_res
-                self.cloc_stats[name] = cloc_res
-            # Cache the results if requested
-            if cache_results:
-                print("Caching results: %s" % self.cache_file_cloc)
-                with open(self.cache_file_cloc, 'w') as outfile:
-                    yaml.dump(self.cloc_stats, outfile)
-                print("Caching results: %s" % self.cache_file_commits)
-                with open(self.cache_file_commits, 'w') as outfile:
-                    yaml.dump(self.commit_stats, outfile)
+        self.git_paths = git_paths
+        # Clean and create output directory
+        self.clean_outdirs(output_dir=self.output_dir,
+                           source_dir=self.source_dir)
+        # Clone all repos
+        print("Cloning all repos...")
+        git_repos = self.clone_repos(repos=self.git_paths, source_dir=self.source_dir)
+        # Compute CLOC and Commit statistics for all repos
+        self.commit_stats = {}
+        self.cloc_stats = {}
+        for name, repo in git_repos.items():
+            print("Compute CLOC stats: %s" % name)
+            commit_res, cloc_res = self.git_repo_stats(
+                repo,
+                cloc_path=cloc_path,
+                output_dir=self.output_dir)
+            self.commit_stats[name] = commit_res
+            self.cloc_stats[name] = cloc_res
+        # Cache the results if requested
+        if cache_results:
+            print("Caching results: %s" % self.cache_file_cloc)
+            with open(self.cache_file_cloc, 'w') as outfile:
+                yaml.dump(self.cloc_stats, outfile)
+            print("Caching results: %s" % self.cache_file_commits)
+            with open(self.cache_file_commits, 'w') as outfile:
+                yaml.dump(self.commit_stats, outfile)
+            print("Caching results: %s" % self.cache_git_paths)
+            with open(self.cache_git_paths, 'w') as outfile:
+                yaml.dump(self.git_paths, outfile)
 
     def compute_summary_stats(self, date_range):
         """
@@ -105,7 +143,8 @@ class GitCodeStats:
         our common time axis. For dates where no new CLOC stats are recorded for a repo, the statistics
         from the previous time are carried forward to fill in the gaps.
 
-        :param date_range:
+        :param date_range: Pandas datarange object for which the stats should be computed
+        :type pandas.date_range
         :return: None. The function initalizes self.summary_stats
 
         """
@@ -196,11 +235,57 @@ class GitCodeStats:
         repo_nfiles_aligned_df = pd.DataFrame.from_dict(repo_codes_aligned)
         repo_nfiles_aligned_df.index = date_range
 
-        self.summary_stats = {'sizes': repo_sizes_aligned_df,
-                              'blanks': repo_blanks_aligned_df,
-                              'codes': repo_codes_aligned_df,
-                              'comments': repo_comments_aligned_df,
-                              'nfiles': repo_nfiles_aligned_df}
+        return {'sizes': repo_sizes_aligned_df,
+                'blanks': repo_blanks_aligned_df,
+                'codes': repo_codes_aligned_df,
+                'comments': repo_comments_aligned_df,
+                'nfiles': repo_nfiles_aligned_df}
+
+    def compute_language_stats(self, ignore_lang=None):
+        """
+
+        :param ignore_lang: List of languages to ignore. Usually ['SUM', 'header'] are useful to ignore.
+
+        :return: Dictionary of pandas.DataFrame objects with the language stats for the different repos
+        """
+
+        ignore_lang = [] if ignore_lang is None else ignore_lang
+        languages_used_all = np.unique([lang for v in self.cloc_stats.values() for cl in v for lang in cl['cloc'].keys() if lang not in ignore_lang])
+        per_repo_lang_stats = {}
+        for k, v in self.cloc_stats.items():
+            # languages used in the current repo
+            languages_used = np.unique([lang for cl in v for lang in cl['cloc'].keys() if lang not in ignore_lang])
+            # linear range of dates across the lifetime of this repo
+            date_range_used = pd.date_range(start=self.cloc_stats[k][-1]['date'], end=time.strftime("%d %b %Y", time.localtime()), freq="D")
+            curr_index = 0  # start index in the CLOC data available for the repo
+            curr_values = {l: 0 for l in languages_used}  # current values to be used
+            curr_dates = pd.pandas.DatetimeIndex([cloc_entry['date'] for cloc_entry in v])[::-1] # dates available in the repo
+            curr_stats = {lang: [] for lang in languages_used}
+            # iterate through all date values and set the repo counts
+            for d in date_range_used:
+                # If we found a matching date, then update the results
+                # Else we'll carry-forward the previous value since the
+                # repo has not changed
+                if d == curr_dates[curr_index]:
+                    # Update the current values to report until we find curr_dates[curr_index+1]
+                    for lang, val in v[curr_index]['cloc'].items():
+                        if lang in curr_values:  # e.g., SUM is being ignored
+                            curr_values[lang] = val['blank'] + val['code'] + val['code']
+                    # Move to the next date in the repo
+                    if curr_index < (len(curr_dates) -1):
+                        curr_index += 1
+                # Copy the current values into our curr_stats dict
+                for cl, cv in curr_values.items():
+                    curr_stats[cl].append(cv)
+            # Now that we have our stats lets convert them to pandas
+            per_repo_lang_stats[k] = pd.DataFrame.from_dict(curr_stats)
+            per_repo_lang_stats[k].index = date_range_used[::-1]
+
+        return per_repo_lang_stats
+
+    def get_languages_used(self):
+        """Get the list of languages used in the repos"""
+        return np.unique([lang for v in self.cloc_stats.values() for cl in v for lang in cl['cloc'].keys() if lang not in ignore_lang])
 
 
     @staticmethod
