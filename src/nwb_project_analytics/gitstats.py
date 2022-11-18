80c17b75494ce9d2c2d72878bb875c1ef1794326
Oliver Ruebel <oruebel@lbl.gov>
2022-11-17 16:21:35 -0800

    Add issue analytics, update response time analysis, update GitRepos

--- src/nwb_project_analytics/gitstats.py
+++ src/nwb_project_analytics/gitstats.py
@@ -38,9 +38,9 @@ class IssueLabel(NamedTuple):
         to assign a category, priority, or topic to an issue.
 
         :returns: str with the type or None in case the label does not have a category (i.e., if the label
-                  does not contain a ':' to separate the type and level).
+                  does not contain a ":" to separate the type and level).
         """
-        if ':' in self.label:
+        if ":" in self.label:
             return self.label.split(":")[0]
         return None
 
@@ -51,9 +51,9 @@ class IssueLabel(NamedTuple):
         e.g., critical, high, medium, low level as part of the priority type.
 
         :returns: str with the level or None in case the label does not have a level (e.g.,  if the label
-                  does not contain a ':' to separate the type and level.
+                  does not contain a ":" to separate the type and level.
         """
-        if ':' in self.label:
+        if ":" in self.label:
             label_parts = self.label.split(":")
             if len(label_parts) > 1 and len(label_parts[1]) > 0:
                 return label_parts[1]
@@ -126,51 +126,101 @@ class GitRepo(NamedTuple):
     docs: str = None
     """Online documentation for the software"""
 
+    logo: str = None
+    """URL with the PNG of the logo for the repository"""
+
     @property
     def github_path(self):
         return "https://github.com/%s/%s.git" % (self.owner, self.repo)
 
-    def get_issues_as_dataframe(self, date_threshold, github_obj, tqdm=None):
-        """
-        Get a dataframe for all issues with comments later than the given data
+    @staticmethod
+    def compute_issue_time_of_first_response(issue):
+        """For a given GitHub issue compute the time to first respone based on the the issue's timeline"""
+        response_time = pd.NaT
+        for event in issue.get_timeline():
+            # Valid initial responses are:
+            # 1) The issue was closed (independent of who did it)
+            # 2) Someone other than the creator of the issue commented or labeled the issue
+            if (event.actor != issue.user and event.event in ["commented", "labeled"]) or event.event == "closed":
+                response_time = event.created_at
+                break  # the timeline is sorted so we can stop once we found the first relevant response
+        return response_time
 
-        :param date_threshold: Datetime object with data of latest comment threshold
+    def get_issues_as_dataframe(self,
+                                since,
+                                github_obj,
+                                tqdm=None):
+        """
+        Get a dataframe for all issues with updates later than the given data
+
+        :param since: Datetime object with the date of the oldest issue to retrieve
         :param github_obj: PyGitHub github.Github object to use for retrieving issues
         :param tqdm: Supply the tqdm progress bar class to use
-        :return:
+        :return: Pandas DataFrame with the issue data
         """
-        issue_attrs = ['id', 'number', 'user', 'created_at',
-                       'closed_at', 'state', 'title', 'milestone', 'labels',
-                       'pull_request', 'closed_by', 'assignees', 'url']
-        custom_issue_attrs = ['user_login', 'response_time', 'time_to_response',
-                              'time_to_response_f', 'is_enhancement', 'is_help_wanted']
-        issues = github_obj.get_repo("%s/%s" % (self.owner, self.repo)).get_issues(since=date_threshold)
-        curr_df = pd.DataFrame(columns=(issue_attrs + custom_issue_attrs))
+        issue_attrs = ["id", "number", "user", "created_at", "updated_at",
+                       "closed_at", "state", "title", "milestone", "labels", "comments",
+                       "pull_request", "closed_by", "assignees", "url", "locked"]
+        custom_issue_attrs = ["user_login", "response_time", "time_to_response",
+                              "days_to_response", "is_enhancement", "is_help_wanted"]
+        issues = github_obj.get_repo("%s/%s" % (self.owner, self.repo)).get_issues(since=since, state="all")
+        curr_df = pd.DataFrame(columns=(issue_attrs + custom_issue_attrs + ["issue",]))
+        # Convert bool-type columns to bool to avoid Pandas deprecation warnings
+        curr_df.is_enhancement = curr_df.is_enhancement.astype("bool")
+        curr_df.is_help_wanted = curr_df.is_help_wanted.astype("bool")
+        curr_df.locked = curr_df.locked.astype("bool")
+        # Setup progress bar if necessary
         if tqdm is not None:
-            vals = tqdm(issues, position=1, total=issues.totalCount, desc='%s issues' % self.repo)
+            vals = tqdm(issues, position=1, total=issues.totalCount, desc="%s issues" % self.repo)
         else:
             vals = issues
+        # Add the rows (i.e., issues) to the dataframe
         for issue in vals:
             curr_row = {k: getattr(issue, k) for k in issue_attrs}
-            curr_row['response_time'] = pd.NaT
-            curr_row['user_login'] = curr_row['user'].login
-            curr_row['is_enhancement'] = np.any([label.name == "enhancement" for label in curr_row['labels']])
-            curr_row['is_help_wanted'] = np.any([label.name == "help wanted" for label in curr_row['labels']])
-            if issue.comments:
-                for comment in issue.get_comments():
-                    if issue.user != comment.user:  # don't count it if the user commented on their own issue
-                        if pd.isnull(curr_row['response_time']) or curr_row['response_time'] > comment.created_at:
-                            curr_row['response_time'] = comment.created_at
-            # if user closes their own issue, count it as resolved
-            if issue.closed_by == issue.user and issue.closed_at is not None:
-                curr_row['response_time'] = np.min([curr_row['response_time'], issue.closed_at])
-            curr_row['time_to_response'] = pd.to_timedelta(curr_row['response_time'] - curr_row['created_at'])
-            curr_row['time_to_response_f'] = curr_row['time_to_response'] / np.timedelta64(1, 'D')
-            curr_df = curr_df.append(curr_row, ignore_index=True)
-        curr_df.is_enhancement = curr_df.is_enhancement.astype('bool')
-        curr_df.is_help_wanted = curr_df.is_help_wanted.astype('bool')
+            curr_row["issue"] = issue
+            curr_row["response_time"] = pd.NaT
+            curr_row["user_login"] = curr_row["user"].login
+            curr_row["is_enhancement"] = np.any([label.name == "enhancement" for label in curr_row["labels"]]).astype("bool")
+            curr_row["is_help_wanted"] = np.any([label.name == "help wanted" for label in curr_row["labels"]]).astype("bool")
+            curr_row["response_time"] = self.compute_issue_time_of_first_response(issue)
+            curr_row["time_to_response"] = pd.to_timedelta(curr_row["response_time"] - curr_row["created_at"])
+            curr_row["days_to_response"] = curr_row["time_to_response"] / np.timedelta64(1, "D")
+            curr_df = pd.concat([curr_df, pd.DataFrame([curr_row])], axis=0, join="outer", ignore_index=True)
         return curr_df
 
+    def get_commits_as_dataframe(self,
+                                 since,
+                                 github_obj,
+                                 tqdm):
+        """
+        Get a dataframe for all commits with updates later than the given data
+
+        :param since: Datetime object with the date of the oldest issue to retrieve
+        :param github_obj: PyGitHub github.Github object to use for retrieving issues
+        :param tqdm: Supply the tqdm progress bar class to use
+        :return: Pandas DataFrame with the commits data
+        """
+        commits = github_obj.get_repo("%s/%s" % (self.owner, self.repo)).get_commits(since=since)
+
+        commit_attrs = ["author", "committer", "url", "html_url", "files"]
+        custom_commit_attrs = ["deletions", "additions", "total", "date", "message"]
+        curr_df = pd.DataFrame(columns=(commit_attrs + custom_commit_attrs + ["commit", ]))
+
+        if tqdm is not None:
+            vals = tqdm(commits, position=1, total=commits.totalCount, desc="%s commits" % self.repo)
+        else:
+            vals = commits
+        for commit in vals:
+            curr_row = {k: getattr(commit, k) for k in commit_attrs}
+            commit_stats = commit.stats
+            curr_row["deletions"] = commit_stats.deletions
+            curr_row["additions"] = commit_stats.additions
+            curr_row["total"] = commit_stats.total
+            curr_row["date"] = datetime.strptime(commit.raw_data["commit"]["committer"]["date"], "%Y-%m-%dT%H:%M:%SZ")
+            curr_row["message"] = commit.commit.message
+            curr_row["commit"] = commit
+            curr_df = pd.concat([curr_df, pd.DataFrame([curr_row])], axis=0, join="outer", ignore_index=True)
+        return curr_df
 
 class GitRepos(OrderedDict):
     """Dict where the keys are names of codes and the values are GitRepo objects"""
@@ -180,6 +230,13 @@ class GitRepos(OrderedDict):
     def get_info_objects(self):
         """Get an OrderedDict of GitHubRepoInfo object from the repos"""
         return OrderedDict([(k, GitHubRepoInfo(v)) for k, v in self.items()])
+
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            sel_keys = list(self.keys())[item]
+            return GitRepos([(k, self[k]) for k in sel_keys])
+        else:
+            return super().__getitem__(item)
 
     @staticmethod
     def merge(o1, o2):
@@ -249,93 +306,111 @@ class NWBGitInfo:
           GitRepo(owner="NeurodataWithoutBorders",
                   repo="pynwb",
                   mainbranch="dev",
-                  docs="https://pynwb.readthedocs.io")),
+                  docs="https://pynwb.readthedocs.io",
+                  logo="https://raw.githubusercontent.com/NeurodataWithoutBorders/pynwb/dev/docs/source/figures/logo_pynwb.png")),
          ("MatNWB",
           GitRepo(owner="NeurodataWithoutBorders",
                   repo="matnwb",
-                  mainbranch='master',
-                  docs="https://neurodatawithoutborders.github.io/matnwb/")),
+                  mainbranch="master",
+                  docs="https://neurodatawithoutborders.github.io/matnwb/",
+                  logo="https://raw.githubusercontent.com/NeurodataWithoutBorders/matnwb/master/logo/logo_matnwb.png")),
          ("NWBWidgets",
           GitRepo(owner="NeurodataWithoutBorders",
                   repo="nwb-jupyter-widgets",
-                  mainbranch='master',
-                  docs=None)),
+                  mainbranch="master",
+                  docs=None,
+                  logo=None)),
          ("NWBInspector",
           GitRepo(owner="NeurodataWithoutBorders",
                   repo="nwbinspector",
                   mainbranch="dev",
-                  docs="https://nwbinspector.readthedocs.io")),
+                  docs="https://nwbinspector.readthedocs.io",
+                  logo="https://raw.githubusercontent.com/NeurodataWithoutBorders/nwbinspector/dev/docs/logo/logo.png")),
          ("Hackathons",
           GitRepo(owner="NeurodataWithoutBorders",
                   repo="nwb_hackathons",
                   mainbranch="main",
-                  docs="https://neurodatawithoutborders.github.io/nwb_hackathons/")),
+                  docs="https://neurodatawithoutborders.github.io/nwb_hackathons/",
+                  logo=None)),
          ("NWB_Overview",
           GitRepo(owner="NeurodataWithoutBorders",
                   repo="nwb-overview",
                   mainbranch="main",
-                  docs="https://nwb-overview.readthedocs.io")),
+                  docs="https://nwb-overview.readthedocs.io",
+                  logo=None)),
          ("NWB_Schema",
           GitRepo(owner="NeurodataWithoutBorders",
                   repo="nwb-schema",
                   mainbranch="dev",
-                  docs="https://nwb-schema.readthedocs.io")),
+                  docs="https://nwb-schema.readthedocs.io",
+                  logo=None)),
          ("NWB_Schema_Language",
           GitRepo(owner="NeurodataWithoutBorders",
                   repo="nwb-schema-language",
                   mainbranch="main",
-                  docs="https://schema-language.readthedocs.io")),
+                  docs="https://schema-language.readthedocs.io",
+                  logo=None)),
          ("HDMF",
           GitRepo(owner="hdmf-dev",
                   repo="hdmf",
                   mainbranch="dev",
-                  docs="https://hdmf.readthedocs.io")),
+                  docs="https://hdmf.readthedocs.io",
+                  logo="https://raw.githubusercontent.com/hdmf-dev/hdmf/dev/docs/source/hdmf_logo.png")),
          ("HDMF_Zarr",
           GitRepo(owner="hdmf-dev",
                   repo="hdmf-zarr",
                   mainbranch="dev",
-                  docs="https://hdmf-zarr.readthedocs.io")),
+                  docs="https://hdmf-zarr.readthedocs.io",
+                  logo="https://raw.githubusercontent.com/hdmf-dev/hdmf-zarr/dev/docs/source/figures/logo_hdmf_zarr.png")),
          ("HDMF_Common_Schema",
           GitRepo(owner="hdmf-dev",
                   repo="hdmf-common-schema",
                   mainbranch="main",
-                  docs="https://hdmf-common-schema.readthedocs.io")),
+                  docs="https://hdmf-common-schema.readthedocs.io",
+                  logo=None)),
          ("HDMF_DocUtils",
           GitRepo(owner="hdmf-dev",
                   repo="hdmf-docutils",
                   mainbranch="main",
-                  docs=None)),
+                  docs=None,
+                  logo=None)),
          ("HDMF_Schema_Language",
           GitRepo(owner="hdmf-dev",
                   repo="hdmf-schema-language",
                   mainbranch="main",
-                  docs="https://hdmf-schema-language.readthedocs.io/")),
+                  docs="https://hdmf-schema-language.readthedocs.io/",
+                  logo=None)),
          ("NDX_Template",
           GitRepo(owner="nwb-extensions",
                   repo="ndx-template",
                   mainbranch="main",
-                  docs="https://github.com/nwb-extensions/ndx-template#getting-started")),
+                  docs="https://github.com/nwb-extensions/ndx-template#getting-started",
+                  logo=None)),
          ("NDX_Staged_Extensions",
           GitRepo(owner="nwb-extensions",
                   repo="staged-extensions",
-                  mainbranch='master',
-                  docs=None)),
+                  mainbranch="master",
+                  docs=None,
+                  logo=None)),
          # "NDX Webservices", "https,//github.com/nwb-extensions/nwb-extensions-webservices.git",
          ("NDX_Catalog",
           GitRepo(owner="nwb-extensions",
                   repo="nwb-extensions.github.io",
                   mainbranch="main",
-                  docs="https://nwb-extensions.github.io/")),
+                  docs="https://nwb-extensions.github.io/",
+                  logo="https://github.com/nwb-extensions/nwb-extensions.github.io/blob/main/images/ndx-logo-text.png")),
          ("NDX_Extension_Smithy",
           GitRepo(owner="nwb-extensions",
                   repo="nwb-extensions-smithy",
-                  mainbranch='master',
-                  docs=None)),
+                  mainbranch="master",
+                  docs=None,
+                  logo=None)),
          ("NeuroConv",
           GitRepo(owner="catalystneuro",
                   repo="neuroconv",
                   mainbranch="main",
-                  docs="https://neuroconv.readthedocs.io"))
+                  docs="https://neuroconv.readthedocs.io",
+                  logo=None))
          ])
     """
     Dictionary with main NWB git repositories. The values are GitRepo tuples with the owner and repo name.
@@ -345,43 +420,28 @@ class NWBGitInfo:
         [("NWB_1.x_Matlab",
           GitRepo(owner="NeurodataWithoutBorders",
                   repo="api-matlab",
-                  mainbranch="dev")),
+                  mainbranch="dev",
+                  logo=None)),
          ("NWB_1.x_Python",
           GitRepo(owner="NeurodataWithoutBorders",
                   repo="api-python",
-                  mainbranch="dev"))
+                  mainbranch="dev",
+                  logo=None))
          ])
     """
     Dictionary with main NWB 1.x git repositories. The values are GitRepo tuples with the owner and repo name.
     """
 
-    CORE_API_REPOS = GitRepos(
-        [("PyNWB",
-          GitRepo(owner="NeurodataWithoutBorders",
-                  repo="pynwb",
-                  mainbranch="dev",
-                  docs="https://pynwb.readthedocs.io")),
-         ("HDMF",
-          GitRepo(owner="hdmf-dev",
-                  repo="hdmf",
-                  mainbranch="dev",
-                  docs="https://hdmf.readthedocs.io")),
-         ("MatNWB",
-          GitRepo(owner="NeurodataWithoutBorders",
-                  repo="matnwb",
-                  mainbranch='master',
-                  docs="https://neurodatawithoutborders.github.io/matnwb/")),
-         ("NWB_Schema",
-          GitRepo(owner="NeurodataWithoutBorders",
-                  repo="nwb-schema",
-                  mainbranch="dev",
-                  docs="https://nwb-schema.readthedocs.io"))
-         ])
-    """
-    Dictionary with the main NWB git repos related the user APIs.
-    """
+    @classmethod
+    @property
+    def CORE_API_REPOS(cls):
+        """
+        Dictionary with the main NWB git repos related the user APIs.
+        """
+        return GitRepos([(k, cls.GIT_REPOS[k]) for k in ["PyNWB", "HDMF", "MatNWB", "NWB_Schema"]])
 
-    CORE_DEVELOPERS = ['rly', 'bendichter', 'oruebel', 'ajtritt', 'ln-vidrio', 'mavaylon1']
+
+    CORE_DEVELOPERS = ["rly", "bendichter", "oruebel", "ajtritt", "ln-vidrio", "mavaylon1", "CodyCBakerPhD"]
     """
     List of names of the core developers of NWB overall. These are used, e.g., when analyzing issue stats as
     core developer issues should not count against user issues.
@@ -497,7 +557,7 @@ class GitHubRepoInfo:
         dates = []
         for rel in releases:
             if "Latest" not in rel["name"]:
-                names.append(rel["tag_name"].lstrip('v'))
+                names.append(rel["tag_name"].lstrip("v"))
                 dates.append(rel["published_at"])
         dates = [datetime.strptime(d[0:10], "%Y-%m-%d") for d in dates]
         return names, dates
@@ -517,9 +577,9 @@ class GitHubRepoInfo:
             else:
                 return "patch"
         versions = sorted([LooseVersion(t) for t in tags])
-        # The first version needs to be treated seprately since we don't have a version to compare to
+        # The first version needs to be treated separately since we do not have a version to compare to
         version_jumps = OrderedDict([(versions[0].vstring,
-                                      'major' if versions[0].version[0] > 0 else 'minor')])
+                                      "major" if versions[0].version[0] > 0 else "minor")])
         # Compare current and previous version to determine version jumps
         version_jumps.update(OrderedDict([(versions[i].vstring,
                                            compare_versions(versions[i-1], versions[i]))
