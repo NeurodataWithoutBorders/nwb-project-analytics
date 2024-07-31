a3fab5832f7b14a3cb8ef78114ee47630e92160c
Oliver Ruebel <oruebel@users.noreply.github.com>
2024-07-30 18:19:30 -0700

    Update src/nwb_project_analytics/dandistats.py
    Co-authored-by: Ben Dichter <ben.dichter@gmail.com>

--- src/nwb_project_analytics/dandistats.py
+++ src/nwb_project_analytics/dandistats.py
@@ -243,7 +243,7 @@ class DANDIStats:
                 linewidth=0.5)
         for label in ax.get_xticklabels(which='major'):
             label.set(rotation=30, horizontalalignment='right')
-        ax.set_ylabel('# of NWB dandisets')
+        ax.set_ylabel('# of NWB Dandisets')
         ax.set_xlabel("Date created")
         # Formatting the x-axis to show dates correctly
         plt.gca().xaxis.set_major_locator(mpl_dates.DayLocator(interval=180))
